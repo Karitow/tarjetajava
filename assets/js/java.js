c2673724a6e76c87ef52f7ae4fe1836506866db4
Karol Rivaas <algo.delicado@gmail.com>
2023-11-12 23:14:34 -0300

    mayusculas

--- assets/js/java.js
+++ assets/js/java.js
@@ -4,22 +4,10 @@ let mas = document.querySelector("#mas");
 let menos = document.querySelector("#menos");
 let pagar = document.querySelector("#valor-total");
 
-/* mas.onclick= function () {
-total.InnerHTML =+1; // +1
-valor.InnerHTML = ( 
-    total.InnerHTML * precioInicial.InnerHTML
-    ).tulocalstring ()
-};
-
-menos.onclick= function () {
-total.InnerHTML =-1 ;// -1
-valor.InnerHTML = ( 
-    total.InnerHTML * precioInicial.InnerHTML)
-}; */
 
 menos.addEventListener("click", function () {
 cuantos.innerHTML --;
-pagar.InnerHTML = ( total.innerHTML * precioInicial.innerHTML)
+pagar.innerHTML = ( total.innerHTML * precioInicial.innerHTML)
 })
 mas.addEventListener("click", function () {
 cuantos.innerHTML ++;
